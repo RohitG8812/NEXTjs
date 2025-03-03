105a6563d2f19902c3feae2cda31b00cac2f4c86
Rohit Gupta <Rohitg8812@gmail.com>
2025-03-03 11:47:28 +0530

    custom layout

--- 03-nested-routing/src/app/about/layout.js
+++ 03-nested-routing/src/app/about/layout.js
@@ -6,19 +6,24 @@ import { usePathname } from 'next/navigation'
 
 
 function layout({ children }) {
+    const pages = ['/about/aboutCollege', '/about/aboutStudent']
     const pathName = usePathname()
     console.log(pathName)
     return (
         <div>
-            {pathName != "/about/aboutStudent" ? <ul className='login-list'>
-                <li><Link href={'/'}>Home Page</Link></li>
-                <li><Link href={"/about/aboutCollege"}>College Section</Link></li>
-                <li><Link href={"/about/aboutStudent"}>Student Section</Link></li>
-                <li><Link href={"/about"}>About Page</Link></li>
-            </ul> : <button><Link href={'/about'}>Go to Main login Page</Link></button>}
+            {/* //* Conditional Layout */}
+            {/* {pathName != "/about/aboutStudent" ? */}
+            {!pages.includes(pathName) ?
+                < ul className='login-list'>
+                    <li><Link href={'/'}>Home Page</Link></li>
+                    <li><Link href={"/about/aboutCollege"}>College Section</Link></li>
+                    <li><Link href={"/about/aboutStudent"}>Student Section</Link></li>
+                    <li><Link href={"/about"}>About Page</Link></li>
+                </ul> : <button><Link href={'/about'}>Go to Main login Page</Link></button>
+            }
             {children}
             <h1>Footer</h1>
-        </div>
+        </div >
     )
 }
 
